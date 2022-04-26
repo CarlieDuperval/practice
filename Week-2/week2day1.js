3101164fa77971737f19a8a5b545506e73c3192e
Carlie Duperval <carlierichell@gmail.com>
2022-04-25 21:12:32 -0400

    added update practice for week2day1

--- Week-2/week2day1.js
+++ Week-2/week2day1.js
@@ -28,3 +28,16 @@ const findMinMax = (arr1) => {
     return [Math.min(...arr1), Math.max(...arr1)]
 }
 console.log( 'This is a second way to find Min and Max', findMinMax(minmax))
+
+
+
+// Create a method in the Person class which returns how another person's age compares. Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
+
+// {other person name} is {older than / younger than / the same age as} me.
+
+const compareAge = (p1, p2, p3)=> {
+    if( p1 > p2){
+        console.log()
+    }
+
+}
