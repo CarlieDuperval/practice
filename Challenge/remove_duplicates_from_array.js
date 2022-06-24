6703a0ccc777e58c6660e4e4c947a6d2b96fe617
Carlie Duperval <ritchellcarlie@gmail.com>
2022-06-23 22:15:44 -0400

    remove duplicate from an array

---
+++ Challenge/remove_duplicates_from_array.js
@@ -0,0 +1,33 @@
+//Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
+
+// Examples
+// removeDups([1, 0, 1, 0]) ➞ [1, 0]
+
+// removeDups(["The", "big", "cat"]) ➞ ["The", "big", "cat"]
+
+// removeDups(["John", "Taylor", "John"]) ➞ ["John", "Taylor"]
+// Notes
+// Tests contain arrays with both strings and numbers.
+// Tests are case sensitive.
+// Each array item is unique.
+
+
+// create a function (arr)
+// loop to compare each index
+// 
+
+const myArr = [1, 0, 1, 0, 2, 3, 2, 3, ]
+
+const getUniqueElement = (arr) => {
+    let uniqueArr = []
+   
+    arr.forEach(element => {
+        if(!uniqueArr.includes(element)){
+            uniqueArr.push(element)
+        }
+        
+    });
+    return uniqueArr
+}
+
+console.log(getUniqueElement(myArr))
