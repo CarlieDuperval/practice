484ee5d706ae24e474dae612e98922a267d659e3
Carlie Duperval <ritchellcarlie@gmail.com>
2022-06-27 20:31:24 -0400

    Added basisc javascript

--- BasicsJavaScrtipt/operators.js
+++ BasicsJavaScrtipt/operators.js
@@ -3,6 +3,27 @@ let y = "5" + 5;
 let z = "Hello" + 5;
 
 
-console.log(x)
-console.log(y)
-console.log(z)
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+
+
+let x1 = 5;
+let y1 = 2;
+let z1 = x1 % y1;
+//console.log(z1)
+
+
+let x2 = 5;
+x2--;
+let z2 = x2;
+
+//console.log(z2)
+
+let x3 = 16 + 4 + "Volvo";
+let x4 = "Volvo" + 16 + 4;
+
+console.log(x3)
+console.log(x4)
+
