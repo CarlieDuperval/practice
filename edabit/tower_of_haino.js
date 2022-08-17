f3795e2610078d622633cc532c31e7dee3d01111
Carlie Duperval <ritchellcarlie@gmail.com>
2022-08-17 13:57:02 -0400

    Added Solution of chanllenge towerHanoi

--- edabit/tower_of_haino.js
+++ edabit/tower_of_haino.js
@@ -13,10 +13,18 @@
 // The amount of discs is always a positive integer.
 // 1 disc can be changed per move.
 
-// create a function towerHaino = (disc)=> {}
+// create a function towerHanoi = (disc)=> {}
 	// 2 discs => 3 moves
 	// 3 discs => 7 moves
 	// 4 discs => 15 moves
 	// 5 discs => 31 moves
 	// m = 2^d -1
     // if disc === 0 => return 0  else : return 
+
+    const towerHanoi = (discs) => {
+        if(discs === 0){
+            return 0
+        }
+        return (Math.pow(2, discs)-1)
+    }
+    console.log(towerHanoi(5))
