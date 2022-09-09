0b598ccd4de5442195dfeff0db99ad9bbdda1b9b
Carlie Duperval <ritchellcarlie@gmail.com>
2022-09-09 10:47:49 -0400

    Added expl for challenge removeDuplicate

--- leet-code/removeDuplicateFromArray.js
+++ leet-code/removeDuplicateFromArray.js
@@ -35,13 +35,15 @@
 
 // Input: nums = [1,1,2]
 // Output: 2, nums = [1,2,_]
-// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// Explanation: Your function should return k = 2, 
+//with the first two elements of nums being 1 and 2 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 // Example 2:
 
 // Input: nums = [0,0,1,1,1,2,2,3,3,4]
 // Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// Explanation: Your function should return k = 5, 
+//with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
  
 
@@ -61,4 +63,8 @@ for(let i = 0; i<nums.length; i++){
 }
     return newArr
 }
-console.log(uniqueInteger([2, 4, 4, 4, 5, 5, 6, 7, 7, 8,]))
+console.log(uniqueInteger([1, 1, 1, 2]))
+
+const removeDuplicates = () => {
+    
+}
