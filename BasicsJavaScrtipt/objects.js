d1b480a2514951d18d1dc4a26ad6aa81ad550b2b
Carlie Duperval <ritchellcarlie@gmail.com>
2022-10-04 14:19:21 -0400

    Added practice with C8

---
+++ BasicsJavaScrtipt/objects.js
@@ -0,0 +1,18 @@
+const person = {
+    name: "Carlie",
+    age: 16,
+    address1: 'Boca Raton1',
+    address2: 'Boca Raton2',
+    address3: 'Boca Raton3',
+
+}
+
+console.log('Person is ', person)
+console.log('Person is Called ', person.name)
+// console.log("Person is Called", person[name])
+console.log(`Person's address is `, person.address1)
+
+console.log(`My name is ${person.name}  and
+I'm this old ${person.age}`)
+
+
