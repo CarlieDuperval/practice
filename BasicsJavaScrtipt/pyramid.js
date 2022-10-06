3eaf409d759a6684adb3cfb96bcef02c373a0ac9
Carlie Duperval <ritchellcarlie@gmail.com>
2022-10-05 23:06:36 -0400

    Added pyramid practice

--- BasicsJavaScrtipt/pyramid.js
+++ BasicsJavaScrtipt/pyramid.js
@@ -6,15 +6,53 @@
 // }
 
 
-const printPyramid = (n) => {
-    let result = n 
-    for(let i= 0; i< 10; i++){
-       n += n
+
+function generatePyramid() {
+    var totalNumberofRows = 5;
+    var output = '';
+    for (var i = 1; i <= totalNumberofRows; i++) {
+        for (var j = 1; j <= i; j++) {
+            output += j + '  ';
+        }
+        console.log(output);
+        output = '';
     }
-    return result
+}
+
+generatePyramid();
+
+
+const printPyramid = (n) => {
+    let arr = []
+    for(let i= 0; i<n; i++){
+       for (let j = i+1; j<=i; j++){
+         arr.push(j)
+         console.log(j)
+       }
+       console.log("\n")
+    }
+
+    
 
 } 
-console.log(printPyramid("@"))
+printPyramid(5)
+
+
+
+
+
+const generatePyramid1 = (number) =>{
+    let res = '';
+    for (let i = 1; i <= number; i++) {
+        for (let j = 1; j <= i; j++) {
+            res += j + '  ';
+        }
+        console.log(res);
+        res = '';
+    }
+}
+
+generatePyramid1(8);
 
 
 // let result = '#'
